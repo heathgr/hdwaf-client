313314f3c2210c84c808036b19aa87a2c7b0eaf5
Grant Heath <heathgr@gmail.com>
2016-05-15 20:51:32 -0600

    added a script for generating test coverage reports

---
+++ test.js
@@ -0,0 +1,26 @@
+var spawn = require('child_process').spawn;
+
+process.env.NODE_PATH = __dirname + '/../config/firebase/test/';
+process.env.BABEL_ENV = 'test';
+
+var mochaThread = spawn(
+  'mocha',
+  [
+    '--require',
+    'jsdom.config.js',
+    '--compilers',
+    'js:babel-core/register',
+    '--recursive',
+    'spec/**/*.spec.js',
+    'spec/**/**/*.spec.js',
+    '--colors',
+  ]
+);
+
+mochaThread.stdout.on('data', (data) => {
+  process.stdout.write(data);
+});
+
+mochaThread.stderr.on('data', (data) => {
+  process.stdout.write(data);
+});
